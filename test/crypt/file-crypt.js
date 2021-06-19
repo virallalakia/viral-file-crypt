68636dab77079f88abea7abb6b53be4f73275e31
Viral Lalakia <viral.lalakia@gmail.com>
2021-06-19 21:21:29 +0530

    add various cases for tests

--- test/crypt/file-crypt.js
+++ test/crypt/file-crypt.js
@@ -1,33 +1,57 @@
 const chai = require('chai')
+const chaiFiles = require('chai-files')
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+
+chai.use(chaiFiles);
+
 const expect = chai.expect
+const file = chaiFiles.file
+const tmpDir = path.join(os.tmpdir(), '' + process.hrtime.bigint())
 
 const { cryptFile } = require('../../src/crypt/file-crypt.js')
+const mockSalt = 'mocksalt'
 
 describe('Encrypt a file', () => {
-  const mockSalt = 'mocksalt'
+  const inputFilePath = './test/resources/test-file-1.txt'
+  const outputFilePath = path.join(tmpDir, 'test-file-1-mocksalt-encrypted.output.txt')
+  const expectedOutputFilePath = './test/resources/test-file-1-mocksalt-encrypted.txt'
+
+  before(function() {
+    if (!fs.existsSync(tmpDir)){
+      fs.mkdirSync(tmpDir);
+    }
+  })
   
   it('verify file encrypt is successful', (done) => {
-    cryptFile(mockSalt, './test/resources/test-input-1.txt', './test/resources/test-output-1-mocksalt.txt', function(success) {
+    cryptFile(mockSalt, inputFilePath, outputFilePath, function(success) {
       expect(success).to.be.true
+      expect(file(outputFilePath)).to.exist
       done()
     });
   })
   
   it('verify encrypted file', (done) => {
-    cryptFile(mockSalt, './test/resources/test-input-1.txt', './test/resources/test-output-1-mocksalt-verify-file.txt', function(success) {
+    cryptFile(mockSalt, inputFilePath, outputFilePath, function(success) {
       expect(success).to.be.true
-      // TODO - compare file to verify
+      expect(file(outputFilePath)).to.exist
+      expect(file(outputFilePath)).to.equal(file(expectedOutputFilePath));
       done()
     });
   })
 })
 
 describe('Decrypt a file', () => {
-  const mockSalt = 'mocksalt'
-
+  const inputFilePath = './test/resources/test-file-1-mocksalt-encrypted.txt'
+  const outputFilePath = path.join(tmpDir, 'test-file-1-mocksalt-decrypted.txt')
+  const expectedOutputFilePath = './test/resources/test-file-1.txt'
+  
   it('verify decrypted file', (done) => {
-    cryptFile(mockSalt, './test/resources/test-input-1.txt.crypt', './test/resources/test-output-1-mocksalt.txt.crypt', function(success) {
+    cryptFile(mockSalt, inputFilePath, outputFilePath, function(success) {
       expect(success).to.be.true
+      expect(file(outputFilePath)).to.exist
+      expect(file(outputFilePath)).to.equal(file(expectedOutputFilePath));
       done()
     });
   })
